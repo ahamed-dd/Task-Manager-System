7a8f4488a2c5c115f08f559df3aa92e2f5fb8ad3
Ahamed <192886918+ahamed-dd@users.noreply.github.com>
2025-12-07 14:34:37 +0530

    Resolve variable Issue

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -17,7 +17,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
